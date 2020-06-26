1bc2ae2bb8ba77de2bfed74bb9ad89f01754c03f
TalkativeJin <67410476+TalkativeJin@users.noreply.github.com>
2020-06-26 22:52:47 +0900

    JS : home fade

--- main.js
+++ main.js
@@ -31,3 +31,10 @@ homeContactBtn.addEventListener("click", () => {
   const scrollTo = document.querySelector("#contact");
   scrollTo.scrollIntoView({ behavior: "smooth" });
 });
+
+// Make Home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
